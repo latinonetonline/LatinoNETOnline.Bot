2a97022964d5305f545e31da297667024842381f
lautaro <lautaroecarro@gmail.com>
2020-05-31 01:04:31 -0300

    Fix AnnouncementNextEvent

--- LatinoNETOnline.TelegramBot.Application/UseCases/Announcements/AnnouncementNextEvent/AnnouncementNextEventHandler.cs
+++ LatinoNETOnline.TelegramBot.Application/UseCases/Announcements/AnnouncementNextEvent/AnnouncementNextEventHandler.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 using LatinoNETOnline.TelegramBot.Application.Repositories;
+using LatinoNETOnline.TelegramBot.Application.Services;
 using LatinoNETOnline.TelegramBot.Application.UseCases.Bots.Messages.SendNextEvent;
 
 using MediatR;
@@ -12,22 +14,29 @@ namespace LatinoNETOnline.TelegramBot.Application.UseCases.Announcements.Announc
     {
         private readonly IMediator _mediator;
         private readonly ISubscribedUsersRepository _subscribedUsersRepository;
+        private readonly IEventService _eventService;
 
-        public AnnouncementNextEventHandler(IMediator mediator, ISubscribedUsersRepository subscribedUsersRepository)
+        public AnnouncementNextEventHandler(IMediator mediator, ISubscribedUsersRepository subscribedUsersRepository, IEventService eventService)
         {
             _mediator = mediator;
             _subscribedUsersRepository = subscribedUsersRepository;
+            _eventService = eventService;
         }
 
         protected override async Task Handle(AnnouncementNextEventRequest request, CancellationToken cancellationToken)
         {
-            var users = await _subscribedUsersRepository.GetAll();
+            var @event = await _eventService.GetNextEventAsync();
 
-            foreach (var user in users)
+            if (@event != null && @event.Date > DateTime.Now.ToUniversalTime())
             {
-                SendNextEventRequest sendNextEventRequest = new SendNextEventRequest(user.UserId, null);
-                await _mediator.Send(sendNextEventRequest);
-            }
+                var users = await _subscribedUsersRepository.GetAll();
+
+                foreach (var user in users)
+                {
+                    SendNextEventRequest sendNextEventRequest = new SendNextEventRequest(user.UserId, null);
+                    await _mediator.Send(sendNextEventRequest);
+                }
+            }  
         }
     }
 }
